295a79fb9d952d21968bdb5ba4ad050e1be292bd
Xiaotong Xie <mayxiexiaotong@163.com>
2022-04-04 14:16:37 -0500

    AddUserControllerAndSeedData

---
+++ Project-Manager/Models/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project_Manager.Data;
+
+namespace Project_Manager.Models
+{
+    public class SeedData
+    {
+        public async static Task Initialize(IServiceProvider serviceProvider)
+        {
+            var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            if (!context.Roles.Any())
+            {
+                List<string> seededRoles = new List<string>()
+                {
+                    "Admin",
+                    "Manager",
+                    "Developer"
+                };
+                
+                foreach(string s in seededRoles)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(s));
+                }
+            }
+
+            if (!context.Users.Any())
+            {
+                var passwordHasher = new PasswordHasher<ApplicationUser>();
+
+                ApplicationUser firstAdmin = new ApplicationUser
+                {
+                    Email = "admin@123.ca",
+                    NormalizedEmail = "ADMIN@123.CA",
+                    UserName = "admin@123.ca",
+                    NormalizedUserName = "ADMIN@123.CA",
+                    EmailConfirmed = true
+                };
+
+                var hashedPassword = passwordHasher.HashPassword(firstAdmin, "`1Qazxsw");
+                firstAdmin.PasswordHash = hashedPassword;
+
+                await userManager.CreateAsync(firstAdmin);
+                await userManager.AddToRoleAsync(firstAdmin, "Admin");
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
--- Project-Manager/Program.cs
+++ Project-Manager/Program.cs
@@ -11,24 +11,31 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseMigrationsEndPoint();
+    var services = scope.ServiceProvider;
+
+    await SeedData.Initialize(services);
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
